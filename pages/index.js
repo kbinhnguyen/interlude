727dea25f72d57ea945b47eeb950469ca53600fc
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-08 23:00:37 -0400

    Add config file for Next to disable running fs-module code on the browser. Initial rendering of feed page setup completed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
+import { getAllUsers } from '../server/model.js';
+
 function Feed({ users }) {
-  return (<h1>Welcome to the Project!</h1>);
+  return (
+    <>
+      <h1>Welcome to the Project!</h1>
+      <ul>{users.map((user) => (<li key={user.id}>{user.username}</li>))}</ul>
+    </>
+  );
 }
 
-Feed.getInitialProps = async () => {
-  const users = [1, 2, 3];
-  return { users };
-};
+export async function getServerSideProps() {
+  const results = await getAllUsers();
+  const users = await results.rows;
+  return { props: { users } };
+}
 
 export default Feed;
