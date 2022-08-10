3ad8c0c2dc6b9c207f7379c8d81b082e6f72e8f4
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-10 18:24:30 -0400

    Adjusted styling to remove bullet from list elements of search results

--- components/SearchRes.js
+++ components/SearchRes.js
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
 import { BsFillPlayCircleFill } from 'react-icons/bs';
-// import Audio from './Audio';
 
 function SearchRes({ searchResults }) {
   const [currRef, setCurrRef] = useState(null);
@@ -19,18 +18,30 @@ function SearchRes({ searchResults }) {
 
   if (searchResults && searchResults.length > 0) {
     return (
-      <ul>
-        {searchResults.map((result) => (
-          <li key={result.id}>
-            <BsFillPlayCircleFill onClick={() => { setSelectedTrack(result); }} />
-            <span>{`${result.title} by ${result.artists.join(', ')}`}</span>
-            {selectedTrack
-              && selectedTrack.id === result.id
-              && (
-                <audio src={result.preview} ref={sound} />)}
-          </li>
-        ))}
-      </ul>
+      <>
+        <ul className="searchList">
+          {searchResults.map((result) => (
+            <li key={result.id}>
+              {result.preview
+                && (<BsFillPlayCircleFill onClick={() => { setSelectedTrack(result); }} />)}
+              <span>{`${result.title} by ${result.artists.join(', ')}`}</span>
+              {selectedTrack
+                && selectedTrack.id === result.id
+                && (
+                  <audio src={result.preview} ref={sound} />)}
+            </li>
+          ))}
+        </ul>
+        <style jsx>
+          {`
+            .searchList {
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+            }
+          `}
+        </style>
+      </>
     );
   } else if (searchResults === null) {
     return (<h4>Sorry! Couldn't find anything for ya... 😖</h4>)
