215598dba97773be0890a8f3700296d797d0fb07
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-09 20:03:29 -0400

    Me CSR basic setup completed

---
+++ components/Form.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import graphQLClient from '../components/ApolloClient';
+
+function Form({ searchClicked, setSearchClicked }) {
+  const [querySong, setQuerySong] = useState('');
+  const [queryArtists, setQueryArtists] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Search was clicked');
+    // need to call API here still
+    setSearchClicked(!searchClicked);
+    setQuerySong('');
+    setQueryArtists('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={querySong}
+        onChange={(e) => { setQuerySong(e.target.value); }}
+        placeholder="Song"
+        required
+      />
+      <input
+        type="text"
+        value={queryArtists}
+        onChange={(e) => { setQueryArtists(e.target.value); }}
+        placeholder="Artist(s)"
+        required
+      />
+      <button type="submit">Search!</button>
+    </form>
+  );
+}
+
+export default Form;
--- pages/me.js
+++ pages/me.js
@@ -1,23 +1,44 @@
 import { gql, useApolloClient } from '@apollo/client';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import Form from '../components/Form';
 
 function Me() {
   const client = useApolloClient();
+  const [searchClicked, setSearchClicked] = useState(false);
+  const [tracks, setTracks] = useState([]);
 
   useEffect(() => {
+    console.log('Me was rendered');
     client.query({
       query: gql`query Query {
-        allUsers {
-          username
-          img_url
+        user(id: 1) {
+          tracks_liked {
+            id
+            title
+            artists
+            img_url
+            preview
+            uri
+          }
         }
-      }`,
+      }
+      `,
     })
-      .then((results) => { console.log(results); })
-      .catch((err) => {console.log(err);});
-  }, [client]);
+      .then((results) => {
+        console.log(results);
+        console.log(results.data.user.tracks_liked);
+        setTracks(results.data.user.tracks_liked);
+      })
+      .catch((err) => {console.log(err); });
+  }, [client, searchClicked]);
 
-  return (<h1>This is my personal space!</h1>);
+  return (
+    <>
+      <h1>This is my personal space!</h1>
+      {console.log(searchClicked)}
+      <Form searchClicked={searchClicked} setSearchClicked={setSearchClicked} />
+    </>
+  );
 }
 
 export default Me;
