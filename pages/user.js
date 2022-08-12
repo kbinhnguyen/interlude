5f5ef292d15de1186b3eae0d0e3cbb013bebcec3
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-12 04:47:16 -0400

    Styling for user page completed

--- pages/user.js
+++ pages/user.js
@@ -6,15 +6,79 @@ import Collage from '../components/Collage';
 function User({ userData }) {
   return (
     <>
-      <div className="user-profile">
-        <Image src={userData.img_url} alt={userData.username} width={200} height={200} objectFit="cover" />
-        <h3>{userData.username}</h3>
+      <div className="user-page">
+        <div className="user-profile">
+          <div className="avatar">
+            <Image className="avatar" src={userData.img_url} alt={userData.username} width={200} height={200} objectFit="cover" />
+          </div>
+          <div className="side-profile">
+            <h2 className="text">{`♮ ${userData.username}`}</h2>
+            <h4 className="text">
+              {'Get to know '}
+              <span style={{ textDecoration: 'overline' }}>{userData.username}</span>
+              {' through some tunes '}
+              <span style={{ fontStyle: 'normal', fontSize: '20px' }}>😆</span>
+            </h4>
+            <audio className="audio" src={userData.tracks_liked[0].preview} controls />
+          </div>
+        </div>
+        <Collage tracks={userData.tracks_liked} />
       </div>
-      <Collage tracks={userData.tracks_liked} />
+
       <style jsx>
         {`
+          .user-page{
+            display: grid;
+            grid-auto-rows: max-content;
+          }
           .user-profile {
+            display: grid;
+            grid-template-columns: 3fr 6fr;
+            height: 300px;
+            width: 80%;
+            justify-self: center;
             margin-top: 3rem;
+            align-items: start;
+            background: #FFC857;
+            border-radius: 20px;
+          }
+          .side-profile {
+            width: 90%;
+            align-self: center;
+            display: grid;
+            grid-template-rows: max-content max-content max-content;
+            gap: 15px;
+          }
+          .avatar {
+            border-radius: 50%;
+            overflow: hidden;
+            width: 200px;
+            height: 200px;
+            justify-self: center;
+            align-self: center;
+            border: 1px solid black;
+          }
+          .text {
+            margin: 0;
+          }
+          h4 {
+            font-style: italic;
+          }
+          @media (max-width: 700px) {
+            .user-profile{
+              height: max-content;
+              justify-content: center;
+              justify-items: center;
+              grid-template-rows: 240px 240px;
+              grid-template-columns: 100%;
+            }
+            .side-profile {
+              justify-self: center;
+              justify-items: center;
+            }
+            .avatar {
+              align-self: end;
+            }
           }
         `}
       </style>
